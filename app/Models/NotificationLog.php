21a95a729b4723567b7a414c95842589e5d24616
Rasul Eminli <rasul.eminli@student.wsb.edu.pl>
2026-06-09 13:49:12 +0200

    Update NotificationLog.php

--- app/Models/NotificationLog.php
+++ app/Models/NotificationLog.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationLog extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'type',
+        'title',
+        'message',
+        'amount',
+        'metadata',
+        'is_read',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'metadata' => 'array',
+        'is_read' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
