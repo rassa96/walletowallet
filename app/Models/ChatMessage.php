5bbf8549a2c58b832936678be16e25a30c0c2ccb
Rasul Eminli <rasul.eminli@student.wsb.edu.pl>
2026-06-09 13:44:21 +0200

    Add files via upload

---
+++ app/Models/ChatMessage.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ChatMessage extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'sender_id',
+        'message',
+        'is_from_admin',
+        'is_read',
+    ];
+
+    protected $casts = [
+        'is_from_admin' => 'boolean',
+        'is_read' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+}
